c1bcd12687bc29c1662869e90ea84b2d8ca7cc1f
artisna <tikataka@yandex.ru>
2022-02-03 01:48:34 +0700

    all methods replaced to separate project.

---
+++ SharpDump.Test/DumpTest.cs
@@ -0,0 +1,17 @@
+using Xunit;
+using SharpDump;
+
+namespace SharpDump.Test
+{
+    public class DumpTest
+    {
+        [Fact]
+        public void IsHighIntegrity() 
+        {
+            // arrange
+            // act
+            //asset
+            //var anyResult = SharpDump.
+        }
+    }
+}
