45f72ac1077efc4ea05aab1706a984a329e5fadb
artisna <tikataka@yandex.ru>
2022-02-03 09:37:52 +0700

    added more test for compress

--- SharpDump.Test/DumpTest.cs
+++ SharpDump.Test/DumpTest.cs
@@ -1,10 +1,8 @@
-using Xunit;
-using SharpDump.Logic;
-using Moq;
 using FluentAssertions;
-using System;
-using System.Linq;
+using SharpDump.Logic;
 using System.IO;
+using System.Linq;
+using Xunit;
 
 namespace SharpDump.Test
 {
@@ -21,14 +19,15 @@ namespace SharpDump.Test
         }
 
         [Theory]
-        [InlineData(null, null)]
-        [InlineData("", "")]
-        [InlineData(" ", " ")]
-        [InlineData("input.in", "output.out")]
-        public void Compress_WithInvalidInputOutputFile_ThrowException(string inputFile, string outputFile)
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("input.in")]
+        public void Compress_WithInvalidInput_ThrowException(string inputFile)
         {
             // arrange
             var logger = new StatusLogger();
+            var outputFile = "output.out";
             var expectedLog = "[X] Exception while compressing file: ";
 
             // act
@@ -53,6 +52,48 @@ namespace SharpDump.Test
 
             //assert
             logger.Logs.Should().Contain(expectedLog);
+            File.Exists(existedOutputFilePath).Should().BeFalse();
+        }
+
+        [Fact]
+        public void Compress_WithExistedInputFile_CreateOutput()
+        {
+            // arrange
+            var logger = new StatusLogger();
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var existedInputFilePath = Path.Combine(currentDirectory, "Data", "existedEmptyInput.txt");
+            var outputFilePath = Path.Combine(currentDirectory, "Data", "output.txt");
+
+            // act
+            Dump.Compress(logger, existedInputFilePath, outputFilePath);
+
+            //assert
+            File.Exists(outputFilePath).Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("inputWithContent.txt", "output.zip")]
+        [InlineData("alreadyZippedInput.zip", "output.zip")]
+        [InlineData("inputWithContent.txt", "output.txt")]
+        [InlineData("inputWithContent.txt", "output.out")]
+        public void Compress_WithExistedInputFileWithContent_ZipItToOutput(string inputFileName, string outputFileName)
+        {
+            // arrange
+            var logger = new StatusLogger();
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var existedInputFilePath = Path.Combine(currentDirectory, "Data", inputFileName);
+            var outputFilePath = Path.Combine(currentDirectory, "Data", outputFileName);
+            var inputFileLength = (new FileInfo(existedInputFilePath)).Length;
+
+            // act
+            Dump.Compress(logger, existedInputFilePath, outputFilePath);
+
+            //assert
+            File.Exists(outputFilePath).Should().BeTrue();
+            
+            var outputFileLength = (new FileInfo(outputFilePath)).Length;
+            //inputFileLength.Should().BeGreaterThan(outputFileLength);
+            Assert.NotEqual(inputFileLength, outputFileLength);
         }
     }
 }
